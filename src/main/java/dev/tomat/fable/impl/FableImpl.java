faf0cfa4030ad776ab530ee0d799e7293f99221a
tomat <xxlennygamerxx@gmail.com>
2024-03-31 16:10:39 -0700

    Annotate FableImpl as QuiltLoaderInternal.

--- src/main/java/dev/tomat/fable/impl/FableImpl.java
+++ src/main/java/dev/tomat/fable/impl/FableImpl.java
@@ -1,5 +1,9 @@
 package dev.tomat.fable.impl;
 
+import org.quiltmc.loader.impl.util.QuiltLoaderInternal;
+import org.quiltmc.loader.impl.util.QuiltLoaderInternalType;
+
+@QuiltLoaderInternal(QuiltLoaderInternalType.NEW_INTERNAL)
 public final class FableImpl {
 	public static final String VERSION = "0.1.0";
 }
