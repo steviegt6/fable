0fb730f659f3995d78088f6dfa7a1816f6b7d828
tomat <xxlennygamerxx@gmail.com>
2024-04-01 21:44:53 -0700

    Move around minecraft-test projects.

---
+++ minecraft-test-b1.7.3/src/main/java/net/fabricmc/minecraft/test/server_only/TestMixinGuiHelper.java
@@ -0,0 +1,11 @@
+package net.fabricmc.minecraft.test.server_only;
+
+import org.quiltmc.loader.api.minecraft.DedicatedServerOnly;
+
+@DedicatedServerOnly
+public class TestMixinGuiHelper {
+
+	public static void help() {
+
+	}
+}
