f961c0646041307d915f45a5d375a45cd18753c7
tomat <xxlennygamerxx@gmail.com>
2024-04-05 01:24:29 -0700

    Make the patches BETTER.

--- providers/bta/patches/src/main/java/dev/tomat/modmenu_patches/patches/ModMenuPatch.java
+++ providers/bta/patches/src/main/java/dev/tomat/modmenu_patches/patches/ModMenuPatch.java
@@ -8,23 +8,39 @@ import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public final class ModMenuPatch extends Patch {
+	private static final String[] CANDIDATES = {
+			"io.github.prospector.modmenu.util.BadgeRenderer::draw",
+			"io.github.prospector.modmenu.ModMenu::onInitialize",
+			"io.github.prospector.modmenu.gui.ModListEntry::createIcon",
+	};
+
 	@Override
 	public void process(PatchContext context) {
-		if (!context.getClassName().equals("io.github.prospector.modmenu.util.BadgeRenderer")) {
-			return;
-		}
+		for (String candidate : CANDIDATES) {
+			String[] parts = candidate.split("::");
 
+			if (parts.length != 2) {
+				continue;
+			}
+
+			if (context.getClassName().equals(parts[0])) {
+				processCandidate(context, parts[1]);
+			}
+		}
+	}
+
+	private void processCandidate(PatchContext context, String methodName) {
 		ClassNode badgeRenderer = context.getClassNode();
-		MethodNode draw = findMethod(badgeRenderer, method -> method.name.equals("draw"));
+		MethodNode draw = findMethod(badgeRenderer, method -> method.name.equals(methodName));
 
 		if (draw == null) {
 			return;
 		}
 
 		boolean patched = false;
+		AbstractInsnNode node;
 
-		AbstractInsnNode node = findInsn(draw, insn -> insn instanceof LdcInsnNode && ((LdcInsnNode) insn).cst.equals("minecraft"), false);
-		if (node instanceof LdcInsnNode) {
+		while ((node = findInsn(draw, insn -> insn instanceof LdcInsnNode && ((LdcInsnNode) insn).cst.equals("minecraft"), false)) instanceof LdcInsnNode) {
 			((LdcInsnNode) node).cst = "bta";
 			patched = true;
 		}
